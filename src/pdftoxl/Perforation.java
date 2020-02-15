85c1d6f33fa9ff0acd9d4af416c3627276f593e4
prashanth_mani <prashanth_mani@192.168.1.12>
2020-02-15 08:15:02 +0530

    Added perforation pojo class

---
+++ src/pdftoxl/Perforation.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdftoxl;
+
+/**
+ *
+ * @author prashanth_mani
+ */
+public class Perforation {
+
+    /**
+     * @return the Well_ID
+     */
+    public String getWell_ID() {
+        return Well_ID;
+    }
+
+    /**
+     * @param Well_ID the Well_ID to set
+     */
+    public void setWell_ID(String Well_ID) {
+        this.Well_ID = Well_ID;
+    }
+
+    /**
+     * @return the From
+     */
+    public String getFrom() {
+        return From;
+    }
+
+    /**
+     * @param From the From to set
+     */
+    public void setFrom(String From) {
+        this.From = From;
+    }
+
+    /**
+     * @return the To
+     */
+    public String getTo() {
+        return To;
+    }
+
+    /**
+     * @param To the To to set
+     */
+    public void setTo(String To) {
+        this.To = To;
+    }
+
+    /**
+     * @return the OrderNo
+     */
+    public String getOrderNo() {
+        return OrderNo;
+    }
+
+    /**
+     * @param OrderNo the OrderNo to set
+     */
+    public void setOrderNo(String OrderNo) {
+        this.OrderNo = OrderNo;
+    }
+
+    /**
+     * @return the FractureTreatments
+     */
+    public String getFractureTreatments() {
+        return FractureTreatments;
+    }
+
+    /**
+     * @param FractureTreatments the FractureTreatments to set
+     */
+    public void setFractureTreatments(String FractureTreatments) {
+        this.FractureTreatments = FractureTreatments;
+    }
+
+    /**
+     * @return the AcidVolumes
+     */
+    public String getAcidVolumes() {
+        return AcidVolumes;
+    }
+
+    /**
+     * @param AcidVolumes the AcidVolumes to set
+     */
+    public void setAcidVolumes(String AcidVolumes) {
+        this.AcidVolumes = AcidVolumes;
+    }
+    
+         private String Well_ID;
+         private String From;
+	 private String To;
+	 private String OrderNo;
+	 private String FractureTreatments;
+         private String AcidVolumes;
+	
+    
+    
+}
